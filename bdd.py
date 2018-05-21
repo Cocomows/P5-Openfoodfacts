9bce76f0bf1ba10661105455927a8b4ba1f4745b
Cocomows <corentin.caradec@gmail.com>
2018-05-21 12:46:14 +0200

    code cleaning, beginning of product selection

--- bdd.py
+++ bdd.py
@@ -1,72 +1,206 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+"""
+Module used to handle interactions with the database and display functions
+in the openfoodfact project
+"""
 import math
 import mysql.connector
+import requests
 
 ROWS_PER_PAGE = 30
-NUTRISCORE = {
-              0: 'N/A',
-              1: 'A',
-              2: 'B',
-              3: 'C',
-              4: 'D',
-              5: 'E'
-            }
-CONNECTION_PARAMETERS = {
-                        "host": "localhost",
-                        "user": "cocomo",
-                        "password": "password", 
-                        "database": "openfoodfacts"
-                        }
 
-def display_products_page(category_id, page):
+NUTRISCORE = {
+    0: 'N/A',
+    1: 'A',
+    2: 'B',
+    3: 'C',
+    4: 'D',
+    5: 'E'
+}
+
+CONNECTION_PARAMETERS = {
+    "host": "localhost",
+    "user": "cocomo",
+    "password": "password",
+    "database": "openfoodfacts"
+}
+
+def print_products_page(category_id, page):
+    """
+    Print products of a given category
+    """
+    number_of_rows = get_number_products(category_id)
+    total_pages = get_number_pages(category_id)
+    products = get_products_page(category_id, page)
+
+    if len(products) == 0:
+        print("Aucun résultat")
+        #~ Add option to fill database with results from site
+    else:
+        display_str = '\nDisplaying results {} to {} out of {} (Page {} out of {}):'
+        print(display_str.format(products[0][0], (products[0][0]+len(products)-1),
+                                 number_of_rows, page, total_pages))
+
+        format_string = '{0:>3} |{1:<110}| {2}'
+        print(format_string.format("Num", "Produit - Marque", "Lien Openfoodfacts"))
+        print('='*170)
+
+
+        for index, product in enumerate(products, start=1):
+            product_fullname = str(product[1])+" - "+str(product[2])
+            print(format_string.format(index, product_fullname, product[4]))
+
+    return
+
+def get_products_page(category_id, page):
+    """
+    Request products for a given category at the given page
+    returns a list of products with the following information :
+    product_id, product_name, product_brand, nutriscore,link_openfoodfacts
+    """
     conn = mysql.connector.connect(**CONNECTION_PARAMETERS)
     cursor = conn.cursor()
 
-    #~ Get total number of rows
-    number_of_rows, total_pages = get_row_and_pages(category_id)
-    
     offset = (page-1)*ROWS_PER_PAGE
     #~ Get results
     cursor.execute("""SELECT product_id, product_name, product_brand,
                     nutriscore,link_openfoodfacts FROM product WHERE category_id = %s
-                    ORDER BY product_id ASC LIMIT %s OFFSET %s""", (category_id, ROWS_PER_PAGE, offset))
+                    ORDER BY product_id ASC
+                    LIMIT %s OFFSET %s""", (category_id, ROWS_PER_PAGE, offset))
     rows = cursor.fetchall()
 
-    if len(rows) == 0:
-        print("Aucun résultat")
-    else:
-        display_str = '\n\nDisplaying results {} to {} out of {} (Page {} out of {})\n'
-        print(display_str.format(rows[0][0],(rows[0][0]+len(rows)-1),number_of_rows,page,total_pages))
-
-        format_string = '{0:0>3} |{1:70}|{2:70}| {3:5} | {4}'
-        print(format_string.format(" ID", "Produit", "Marque", "Score","Lien"))
-        print('_'*190)
-
-        for row in rows:
-            print(format_string.format(row[0], row[1], row[2], NUTRISCORE[row[3]], row[4]))
-
     conn.close()
-    return
+    return rows
 
-def get_row_and_pages(category_id):
+
+
+def get_number_products(category_id):
+    """
+    Request all products for a given category from the database
+    and returns number of rows
+    """
     conn = mysql.connector.connect(**CONNECTION_PARAMETERS)
     cursor = conn.cursor()
     #~ Get total number of rows
-    cursor.execute("SELECT COUNT(*) from product where category_id = %s",(category_id,))
-    (number_of_rows,)=cursor.fetchone()
+    cursor.execute("SELECT COUNT(*) from product where category_id = %s", (category_id,))
+    (number_of_rows,) = cursor.fetchone()
     conn.close()
-    total_pages = int(math.ceil(number_of_rows / ROWS_PER_PAGE))
-    return number_of_rows, total_pages
+    return number_of_rows
 
+def get_number_pages(category_id):
+    """
+    Gets the number of rows for a product and return number of pages with const ROW_PER_PAGE
+    """
+    return int(math.ceil(get_number_products(category_id) / ROWS_PER_PAGE))
+
+def str_categories():
+    """
+    Returns string with categories formatted
+    """
+    str_cat = ''
+    for category in get_categories():
+        str_cat += ('{0:02} | {1:30} | {2}\n'.format(category[0], category[1], category[2]))
+    return str_cat
 
 def get_categories():
+    """
+    Request categories from the database and returns a list of categories
+    """
     conn = mysql.connector.connect(**CONNECTION_PARAMETERS)
     cursor = conn.cursor()
-    
+
     #~ Display all available categories from the database
     cursor.execute("""SELECT * FROM category""")
     categories = cursor.fetchall()
+    conn.close()
     return categories
+
+
+def insert_db(url_categorie, categorie_id):
+    """This function add products in database for a given category"""
+
+    json_categorie = requests.get(url_categorie+"1.json").json()
+
+    #~ Number of pages calculated with count of products / product displayed per page
+    nb_pages = int(math.ceil(json_categorie["count"] / json_categorie["page_size"]))
+
+    data = []
+
+
+    for page in range(0, nb_pages):
+        json_categorie = requests.get(url_categorie+str(page+1)+".json").json()
+
+        print("-Requesting products of page "+str(page+1)+" out of "+str(nb_pages))
+
+        for product in json_categorie["products"]:
+            try:
+                str_nutr_grade = product["nutrition_grade_fr"]
+                if str_nutr_grade == 'a':
+                    nutriscore = 1
+                if str_nutr_grade == 'b':
+                    nutriscore = 2
+                if str_nutr_grade == 'c':
+                    nutriscore = 3
+                if str_nutr_grade == 'd':
+                    nutriscore = 4
+                if str_nutr_grade == 'e':
+                    nutriscore = 5
+            except KeyError:
+                str_nutr_grade = "N/A"
+                nutriscore = 0
+
+            if product["stores"] == "":
+                stores = "N/A"
+            else:
+                stores = product["stores"]
+
+            link = "https://fr.openfoodfacts.org/produit/"+str(product["code"])
+
+            data.append((
+                product["product_name"],
+                categorie_id,
+                nutriscore,
+                product["ingredients_text"],
+                stores,
+                link,
+                None,
+                product["brands"]
+            ))
+
+    conn = mysql.connector.connect(**CONNECTION_PARAMETERS)
+    cursor = conn.cursor()
+
+    stmt = """INSERT INTO product (product_name, category_id, nutriscore, description, store,
+            link_openfoodfacts, saved, product_brand)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
+
+    cursor.executemany(stmt, data)
+    print("Lignes correctement insérées")
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+def get_product(category_id, index):
+    """
+    Request products for a given category at the given page
+    returns a list of products with the following information :
+    product_id, product_name, product_brand, nutriscore,link_openfoodfacts
+    """
+    conn = mysql.connector.connect(**CONNECTION_PARAMETERS)
+    cursor = conn.cursor()
+
+    #~ Get results
+    cursor.execute("""SELECT product_id, product_name, product_brand,
+                    nutriscore,link_openfoodfacts FROM product WHERE category_id = %s
+                    ORDER BY product_id ASC
+                    LIMIT 1 OFFSET %s""", (category_id, index-1))
+    rows = cursor.fetchall()
+
+    conn.close()
+    return rows
+
+#~ print(get_product(1,6))
 
--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 import sys
 import os
 #~ import mysql.connector
-import bdd 
+from bdd import *
 
 def clear_and_print(txtmenu, txtchoice=False):
     os.system('cls||clear')
@@ -12,40 +12,80 @@ def clear_and_print(txtmenu, txtchoice=False):
     if txtchoice:
         print(txtchoice)
     return
-    
-def select_product(rows, choice_int):
-    text_choice = ("Vous avez choisi la catégorie numéro "+str(rows[choice_int][0])+
-               " : "+str(rows[choice_int][1]))
+
+def select_product(categories, choice_int):
+
+    #~ Display the menu to select a product and handle the choice
+    category_id = categories[choice_int][0]
+    page = 1
+    total_pages = get_number_pages(category_id)
+
+    text_choice = ("Vous avez choisi la catégorie numéro "+str(category_id)+
+                   " : "+str(categories[choice_int][1]))
+    if total_pages > 1:
+        text_choice += ("\nNavigation avec (B)ack et (N)ext pour la pagination")
+
     clear_and_print(text_choice)
-    bdd.display_products_page(rows[choice_int][0], 1)
-    #~ conn = mysql.connector.connect(host="localhost",user="cocomo",password="password", database="openfoodfacts")
-    #~ cursor = conn.cursor()
+    print_products_page(category_id, page)
 
-    #~ cursor.execute("""SELECT product_id, product_name, product_brand, 
-                    #~ nutriscore FROM product WHERE product_id <= %s 
-                    #~ AND category_id = %s""", ("20", str(rows[choice_int][0])))
-    #~ rows = cursor.fetchall()
-    
-    #~ for row in rows:
-        #~ print('{0} : {1} - {2} nutriscore : {3}'.format(row[0], row[1], row[2], row[3]))
+    user_choice = input(">>")
+    while user_choice.lower() != 'q' and user_choice.lower() != 'm':
+        if user_choice.lower() == 'n':
+            #~ Display next page
+            if page+1 <= total_pages:
+                page += 1
+                clear_and_print(text_choice)
+                print_products_page(category_id, page)
+            else:
+                clear_and_print(text_choice)
+                print_products_page(category_id, page)
+                print("Dernière page atteinte")
+
+        elif user_choice.lower() == 'b':
+            #~ Display previous page
+            if page-1 > 0:
+                page -= 1
+                clear_and_print(text_choice)
+                print_products_page(category_id, page)
+            else:
+                clear_and_print(text_choice)
+                print_products_page(category_id, page)
+                print("Première page atteinte")
 
 
-    #~ conn.close()
-    return
+        try:
+            choice_int = int(user_choice)
+
+            if 0 < choice_int < ROWS_PER_PAGE:
+                #~ Correct choice, handle the chosen category
+                print("choix :"+str(choice_int))
+            else:
+                clear_and_print(text_choice)
+                print_products_page(category_id, page)
+                print("Le nombre choisi n'est pas dans la liste")
+
+        except ValueError:
+
+            clear_and_print(text_choice)
+            print_products_page(category_id, page)
+            print("Choix non reconnu, veuillez entrer un nombre, ou Q pour quitter, "+
+                     "M pour revenir au menu principal")
+
+
+        user_choice = input(">>")
+    return user_choice
 
 def select_category():
 
+    #~ Display the menu to select a category and handle the choice
     txt_menu_cat = ("Selectionnez votre catégorie d'aliment à l'aide du numéro, Q pour quitter, "+
-                 "M pour revenir au menu principal:\n")
-
-    categories = bdd.get_categories()
-    for category in categories:
-        txt_menu_cat += ('{0} : {1} - {2}\n'.format(category[0], category[1], category[2]))
+                    "M pour revenir au menu principal:\n")
+    txt_menu_cat += str_categories()
+    categories = get_categories()
     clear_and_print(txt_menu_cat)
-
     user_choice = input(">>")
 
-    while user_choice != 'q' and user_choice != 'm' and user_choice != 'Q' and user_choice != 'M':
+    while user_choice.lower() != 'q' and user_choice.lower() != 'm':
         try:
             choice_int = int(user_choice)-1
 
@@ -57,7 +97,7 @@ def select_category():
 
         except ValueError:
             error = ("Choix non reconnu, veuillez entrer un nombre, ou Q pour quitter, "+
-                   "M pour revenir au menu principal")
+                     "M pour revenir au menu principal")
             clear_and_print(txt_menu_cat, error)
 
         user_choice = input(">>")
@@ -72,23 +112,23 @@ def menu_principal():
     clear_and_print(txt_menu1)
     user_choice = input(">>")
 
-    while user_choice != 'q' and user_choice != 'Q':
+    while user_choice.lower() != 'q':
 
-        if user_choice == '1':
-            #~ Demander la catégorie
-            user_choice = select_category()
-            clear_and_print(txt_menu1)
+        if user_choice.lower() != 'q':
+            if user_choice == '1':
+                #~ Demander la catégorie
+                user_choice = select_category()
 
-        elif user_choice == '2':
-            #~ Retrouver les aliments substitués
-            clear_and_print(txt_menu1, 'Choix 2')
+            elif user_choice == '2':
+                #~ Retrouver les aliments substitués
+                clear_and_print(txt_menu1, 'Choix 2')
 
-        else:
-            clear_and_print(txt_menu1, 'Choix non reconnu')
+            else:
+                clear_and_print(txt_menu1, 'Choix non reconnu')
 
-        if user_choice != 'q' and user_choice != 'Q':
-
-            user_choice = input(">>")
+            if user_choice.lower() != 'q':
+                #~ If user didn't quit in submenu
+                user_choice = input(">>")
 
 
 def main(args):
