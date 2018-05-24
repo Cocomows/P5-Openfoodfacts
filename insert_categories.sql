0a18223c46c673ab333bd534e3ca99df4e30e321
Cocomows <corentin.caradec@gmail.com>
2018-05-24 16:45:27 +0200

    Added sql script to inster categories in db

---
+++ insert_categories.sql
@@ -0,0 +1,7 @@
+INSERT INTO `category` VALUES (1,'Pâtes à tartiner aux noisettes','https://fr.openfoodfacts.org/categorie/pates-a-tartiner-aux-noisettes/'),
+(2,'Ketchup','https://fr.openfoodfacts.org/categorie/ketchup/'),
+(3,'Pains de mie','https://fr.openfoodfacts.org/categorie/pains-de-mie/'),
+(4,"Jus d\'orange",'https://fr.openfoodfacts.org/categorie/jus-d-orange/'),
+(5,'Biscuits secs','https://fr.openfoodfacts.org/categorie/biscuits-secs/'),
+(6,'Beurres','https://fr.openfoodfacts.org/categorie/beurres/'),
+(7,'Flocons de céréales','https://fr.openfoodfacts.org/categorie/flocons-de-cereales/');
